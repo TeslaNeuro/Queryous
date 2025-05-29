714ddecae3baaa412cd82ed06523c4c585ba8d22
Arshia Keshvari <110261434+TeslaNeuro@users.noreply.github.com>
2025-05-29 11:46:13 +0100

    sonic the hedgehog

--- Basic_Wiki_Sum.py
+++ Basic_Wiki_Sum.py
@@ -1,11 +1,17 @@
 # Basic Research Tool using Streamlit and Wikipedia
 # To summarize information about a thing from Wikipedia
 
+# TO RUN:
+# streamlit run Basic_Wiki_Sum.py
+
+# Author: Arshia Keshvari
+# Date: 2025-05-29
+
 import streamlit as st
 import wikipedia
 
 st.title("Basic Research Tool")
 company = st.text_input("Enter a Basic name")
 if company:
-    summary = wikipedia.summary(company, sentences=3)
+    summary = wikipedia.summary(company, sentences=8)
     st.write(summary)
